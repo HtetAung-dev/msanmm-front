b7ddfb8ade9480b41e1770ea37e9cd3b42b3daa1
HtetAung-ITGuy <htetaung.general@gmail.com>
2023-10-13 20:34:21 +0630

    added breadcrumb

---
+++ msanmm/src/Component/BreadcrumbComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-breadcrumbs>
+        <v-breadcrumbs-item v-for="(item, index) in crumbs" :key="index" :disabled="index === crumbs.length - 1"
+            :to="item.to">
+            {{ item.text }}
+        </v-breadcrumbs-item>
+    </v-breadcrumbs>
+</template>
+  
+<script>
+export default {
+    props: {
+        crumbs: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        isLast(index) {
+            return index === this.crumbs.length - 1
+        },
+        selected(crumb) {
+            this.$emit('selected', crumb)
+        }
+    }
+};
+</script>
+   
+<style scoped></style>
